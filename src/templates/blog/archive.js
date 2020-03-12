310379bb4d880329815e804c4e1b6f31bc7aa9af
Leo Nanfara <leo@pulsarmedia.ca>
2020-03-12 13:13:38 -0400

    Archive and Tag templates updated

--- src/templates/blog/archive.js
+++ src/templates/blog/archive.js
@@ -1,23 +1,28 @@
-import React from "react"
-import Layout from "../../components/layout"
-import { Link, graphql } from "gatsby"
-import { SEO } from "../../components"
-import styled from "styled-components"
-import { GatsbyPagination } from "../../components/pagination"
+import React from "react";
+import Layout from "../../components/layout";
+import { Link, graphql } from "gatsby";
+import { SEO } from "../../components";
+import styled from "styled-components";
+import { GatsbyPagination } from "../../components/pagination";
 
 const PostsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin: 20px 0 0;
-`
+`;
 const PostDetails = styled.div`
   margin: 0 0 40px;
   display: block;
-`
+`;
+
+const TagLink = styled(Link)`
+  text-decoration: none;
+  color: blue;
+`;
 
 const Archive = ({ data, pageContext }) => {
-  const { catSlug, humanPageNumber, numberOfPages } = pageContext
-  const { allWordpressPost } = data
+  const { catSlug, humanPageNumber, numberOfPages } = pageContext;
+  const { allWordpressPost } = data;
 
   return (
     <div>
@@ -28,9 +33,20 @@ const Archive = ({ data, pageContext }) => {
           {allWordpressPost.edges.map(post => (
             <PostDetails key={post.node.id}>
               <h3 dangerouslySetInnerHTML={{ __html: post.node.title }} />
-              <small className="post-date">{post.node.date}</small>
-
+              <small className="post-date">
+                {post.node.date} | Author:{" "}
+                <Link to={post.node.author.path}>{post.node.author.name}</Link>
+              </small>
               <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+              <p>
+                {post.node.tags && "Tagged in: "}
+                {post.node.tags &&
+                  post.node.tags.map((tag, index) => (
+                    <TagLink key={index} to={`/tags/${tag.slug}`}>
+                      {tag.name}{" "}
+                    </TagLink>
+                  ))}
+              </p>
               <div>
                 <Link className="read-more" to={`/post/${post.node.slug}`}>
                   Read More
@@ -47,10 +63,10 @@ const Archive = ({ data, pageContext }) => {
         />
       </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default Archive
+export default Archive;
 
 export const pageQuery = graphql`
   query($catId: String, $skip: Int, $limit: Int) {
@@ -66,8 +82,25 @@ export const pageQuery = graphql`
           excerpt
           slug
           date(formatString: "DD, MMM, YYYY")
+          tags {
+            id
+            name
+            slug
+          }
+          categories {
+            id
+            name
+            slug
+          }
+          author {
+            name
+            url
+            link
+            slug
+            path
+          }
         }
       }
     }
   }
-`
+`;
--- src/templates/blog/tags.js
+++ src/templates/blog/tags.js
@@ -1,24 +1,29 @@
-import React from "react"
-import Layout from "../../components/layout"
-import { Link, graphql } from "gatsby"
-import { SEO } from "../../components"
-import styled from "styled-components"
-import { GatsbyPagination } from "../../components/pagination"
+import React from "react";
+import Layout from "../../components/layout";
+import { Link, graphql } from "gatsby";
+import { SEO } from "../../components";
+import styled from "styled-components";
+import { GatsbyPagination } from "../../components/pagination";
 
 const PostsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin: 20px 0 0;
-`
+`;
 const PostDetails = styled.div`
   margin: 0 0 40px;
   display: block;
-`
+`;
+
+const CatLink = styled(Link)`
+  text-decoration: none;
+  color: blue;
+`;
 
 const Tags = ({ data, pageContext }) => {
-  const { tagSlug, humanPageNumber, numberOfPages } = pageContext
-  const { allWordpressPost } = data
-  console.log("numberOfPages", numberOfPages)
+  const { tagSlug, humanPageNumber, numberOfPages } = pageContext;
+  const { allWordpressPost } = data;
+  console.log("numberOfPages", numberOfPages);
   return (
     <div>
       <Layout>
@@ -28,7 +33,18 @@ const Tags = ({ data, pageContext }) => {
           {allWordpressPost.edges.map(post => (
             <PostDetails key={post.node.id}>
               <h3 dangerouslySetInnerHTML={{ __html: post.node.title }} />
-              <small className="post-date">{post.node.date}</small>
+              <small className="post-date">
+                {post.node.date} | Author:{" "}
+                <Link to={post.node.author.path}>{post.node.author.name}</Link>
+              </small>
+              <p>
+                Posted in:{" "}
+                {post.node.categories.map((cat, index) => (
+                  <CatLink key={index} to={`/archive/${cat.slug}`}>
+                    {cat.name}{" "}
+                  </CatLink>
+                ))}
+              </p>
 
               <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
               <div>
@@ -47,10 +63,10 @@ const Tags = ({ data, pageContext }) => {
         />
       </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default Tags
+export default Tags;
 
 export const pageQuery = graphql`
   query($tagId: String, $skip: Int, $limit: Int) {
@@ -66,8 +82,25 @@ export const pageQuery = graphql`
           excerpt
           slug
           date(formatString: "DD, MMM, YYYY")
+          tags {
+            id
+            name
+            slug
+          }
+          categories {
+            id
+            name
+            slug
+          }
+          author {
+            name
+            url
+            link
+            slug
+            path
+          }
         }
       }
     }
   }
-`
+`;

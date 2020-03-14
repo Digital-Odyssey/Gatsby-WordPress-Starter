0d8258d1936a7cbc8271ab21bc7ce88e85221d2c
Leo Nanfara <leo@pulsarmedia.ca>
2020-03-14 15:45:36 -0400

    cta boxed disabled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from "react";
 import SEO from "../components/seo";
 import { Header, Footer } from "../components";
-import { CTABoxes } from "../components/global/cta";
 
 const IndexPage = () => {
   return (
     <Fragment>
       <SEO title={"Home"} />
       <Header />
-      <CTABoxes />
       <Footer />
     </Fragment>
   );

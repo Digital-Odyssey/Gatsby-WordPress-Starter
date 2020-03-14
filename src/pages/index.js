d40507b8b34a2e0f4a308248133e5dd369aa30c4
Leo Nanfara <leo@pulsarmedia.ca>
2020-03-14 16:00:38 -0400

    Code cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import SEO from "../components/seo";
+import { HeroSlider } from "../components/sliders";
 import { Header, Footer } from "../components";
+import { CTABoxes } from "../components/global/cta";
 
 const IndexPage = () => {
   return (
     <Fragment>
       <SEO title={"Home"} />
       <Header />
+      <HeroSlider />
+      <CTABoxes />
       <Footer />
     </Fragment>
   );

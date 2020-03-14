ef0c7fb3ce3854a328e85beca565abae69878ab7
Leo Nanfara <leo@pulsarmedia.ca>
2020-03-14 15:42:09 -0400

    hero slider disabled

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import SEO from "../components/seo";
-import { HeroSlider } from "../components/sliders";
 import { Header, Footer } from "../components";
 import { CTABoxes } from "../components/global/cta";
 
@@ -9,7 +8,6 @@ const IndexPage = () => {
     <Fragment>
       <SEO title={"Home"} />
       <Header />
-      <HeroSlider />
       <CTABoxes />
       <Footer />
     </Fragment>

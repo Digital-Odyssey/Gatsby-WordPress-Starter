6fd38506696b3a19a68b53bf98a20d9b4c498bf0
Leo Nanfara <leo@pulsarmedia.ca>
2020-03-15 15:56:30 -0400

    container wrapper added to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container body">
+      <div className="row">
+        <div className="col-lg-12">
+          <h1>NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+      </div>
+    </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;

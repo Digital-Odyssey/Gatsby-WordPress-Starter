a231e9c82649ff87239b2d91fcc7a4354a2cbc5a
Leo Nanfara <leo@pulsarmedia.ca>
2020-03-19 19:00:02 -0400

    breadcrumb divider fixed

--- src/components/global/breadcrumbs/BreadCrumbStyles.js
+++ src/components/global/breadcrumbs/BreadCrumbStyles.js
@@ -1,22 +1,16 @@
 import styled from "styled-components";
 
+export const BreadCrumbContainer = styled.div`
+  border-bottom: 1px solid #eee;
+`;
+
 export const BreadCrumbWrapper = styled.div`
   font-family: "Teko", Arial, Helvetica, sans-serif;
   font-weight: 600;
   font-size: 0.7rem;
-  margin: 20px 0 20px;
+  margin: 20px 0 0;
   padding: 0 0 20px;
   position: relative;
-
-  ::after {
-    content: "";
-    width: 5000%;
-    height: 1px;
-    background-color: #ddd;
-    position: absolute;
-    left: -1000px;
-    bottom: 0;
-  }
 
   .divider {
     margin: 0 10px 0 10px;
--- src/components/global/breadcrumbs/Breadcrumbs.js
+++ src/components/global/breadcrumbs/Breadcrumbs.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { Link } from "gatsby";
 import { PropTypes } from "prop-types";
-import { BreadCrumbWrapper } from "./BreadCrumbStyles";
+import { BreadCrumbContainer, BreadCrumbWrapper } from "./BreadCrumbStyles";
 
 const Breadcrumbs = ({ parent }) => (
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-12">
-        <BreadCrumbWrapper>
-          <Link to="/">
-            <span>Home</span>
-          </Link>
-          <span className="divider">/</span>
-          {parent ? (
-            <>
-              <Link to={parent.path}>
-                <span dangerouslySetInnerHTML={{ __html: parent.title }} />
-              </Link>
-            </>
-          ) : null}
-        </BreadCrumbWrapper>
+  <BreadCrumbContainer>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <BreadCrumbWrapper>
+            <Link to="/">
+              <span>Home</span>
+            </Link>
+            <span className="divider">/</span>
+            {parent ? (
+              <>
+                <Link to={parent.path}>
+                  <span dangerouslySetInnerHTML={{ __html: parent.title }} />
+                </Link>
+              </>
+            ) : null}
+          </BreadCrumbWrapper>
+        </div>
       </div>
     </div>
-  </div>
+  </BreadCrumbContainer>
 );
 
 Breadcrumbs.propTypes = {
